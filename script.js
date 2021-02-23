85a977c3cda9b8acad26a611a248490cd9151e93
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-02-22 22:25:05 -0500

    event listener for mouse

--- script.js
+++ script.js
@@ -11,11 +11,37 @@ window.addEventListener('resize', function () {
     // ctx.fillRect(10, 20, 150, 50); //Filling a rectangle with (margin left, margin top, width, height)
 });
 
-ctx.fillStyle = 'blue'; //describing a fill
-ctx.strokeStyle = 'red'; //describing a stroke
-ctx.lineWidth = 3; //width of the stroke
-ctx.beginPath(); //start the path
-ctx.arc(75, 75, 50, 0, Math.PI * 2); // (coordinate1, coordinate2, radius, start angle, PIx2)
-ctx.stroke();
-ctx.fill();
+
+const mouse = {
+    x: null,
+    y: null
+}
+//mouse click event listener
+canvas.addEventListener('click', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    drawCircle();
+
+});
+//mouse movement event listener
+canvas.addEventListener('mousemove', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    drawCircle();
+
+});
+
+
+function drawCircle() {
+    //Creating a circle
+    ctx.fillStyle = 'blue'; //describing a fill
+    //ctx.strokeStyle = 'red'; //describing a stroke
+    ctx.lineWidth = 3; //width of the stroke
+    ctx.beginPath(); //start the path
+    ctx.arc(mouse.x, mouse.y, 5, 0, Math.PI * 2); // (coordinate1, coordinate2, radius, start angle, PIx2)
+    //ctx.stroke();
+    ctx.fill();
+}
+
+
 
