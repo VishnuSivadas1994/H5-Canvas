b60df0e29b065d481ce913a81952a13890e71e47
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-02-23 21:04:59 -0500

    rainbow effect

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('canvas1'); //get the canvas tag from html
 const ctx = canvas.getContext('2d'); //create a canvas 2D context
 const particleArray = [];
+let hue = 0;
 
 window.addEventListener('resize', function () {
     //Fixing the canvas size
@@ -20,6 +21,9 @@ const mouse = {
 canvas.addEventListener('click', function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
+    for (let i = 0; i < 10; i++) {
+        particleArray.push(new Particle());
+    }
     //drawCircle();
 
 });
@@ -27,6 +31,9 @@ canvas.addEventListener('click', function (event) {
 canvas.addEventListener('mousemove', function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
+    for (let i = 0; i < 20; i++) {
+        particleArray.push(new Particle());
+    }
     //drawCircle();
 
 });
@@ -35,22 +42,23 @@ canvas.addEventListener('mousemove', function (event) {
 
 class Particle {
     constructor() {
-        // this.x = mouse.x;
-        // this.y = mouse.y;
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.x = mouse.x;
+        this.y = mouse.y;
+        // this.x = Math.random() * canvas.width;
+        // this.y = Math.random() * canvas.height;
         this.size = Math.random() * 15 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
+        this.color = 'hsl(' + hue + ', 100%, 50%)';
     }
     update() {
         this.x += this.speedX;
         this.y += this.speedY;
-        if(this.size > 0.2) this.size -= 0.1;
+        if (this.size > 0.2) this.size -= 0.1;
     }
     draw() {
         //Creating a circle
-        ctx.fillStyle = 'blue'; //describing a fill
+        ctx.fillStyle = this.color; //describing a fill
         //ctx.strokeStyle = 'red'; //describing a stroke
         ctx.lineWidth = 3; //width of the stroke
         ctx.beginPath(); //start the path
@@ -61,20 +69,14 @@ class Particle {
 
 }
 
-function init(){
-    for(let i = 0; i < 100; i++){
-        particleArray.push(new Particle());
 
-    }
-}
-init();
 
-function handleParticles(){
-    for(let i = 0; i < particleArray.length; i++){
+function handleParticles() {
+    for (let i = 0; i < particleArray.length; i++) {
         particleArray[i].update();
         particleArray[i].draw();
-        if(particleArray[i].size <= 0.3){
-            particleArray.splice(i,1);
+        if (particleArray[i].size <= 0.3) {
+            particleArray.splice(i, 1);
             i--;
         }
     }
@@ -82,8 +84,11 @@ function handleParticles(){
 
 
 function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);//clearing the specified area(coord1, coord2, width, height)
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);//clearing the specified area(coord1, coord2, width, height)
+    ctx.fillStyle = 'rgba(0,0,0,0.02)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     handleParticles();
+    hue += 5;
     requestAnimationFrame(animate);
 }
 animate();
